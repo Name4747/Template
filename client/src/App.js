e9405eac3cc0b3495d3b6b7f17263277e042389b
Name4747 <684438@sylvaniastudents.org>
2021-11-29 13:15:37 -0500

    Create React App

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+const c
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+  <span>What is better?</span>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
