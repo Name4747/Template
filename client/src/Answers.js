cc575349fd381be5b3c6c7709d290511d913b318
Name4747 <684438@sylvaniastudents.org>
2021-12-08 14:03:30 -0500

    begin database

--- client/src/Answers.js
+++ client/src/Answers.js
@@ -1,38 +1,91 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Answers = () => {
-    
-    var a2 = {
-        content: "ford",
-        score: 0,
-        used: false
-    }
 
-    const [a1,setA1] = useState(
+    const [answerList,setAnswerList] = useState()
+
+    useEffect(() => {
+        fetch('/things')
+            .then(res => res.json())
+            .then(data => {
+                setAnswerList(data)
+            })
+    },[])
+
+    /*const answerList = [
         {
+            id: 1,
             content: "bmw",
             score: 0,
             used: false
-        }
-    )
-
-    const getNextAnswer = () => {
-        const newAnswer = {
-            content: "fff",
+        },
+        {
+            id: 2,
+            content: "ford",
+            score: 0,
+            used: false
+        },
+        {
+            id: 3,
+            content: "gm",
+            score: 0,
+            used: false
+        },
+        {   
+            id: 4,
+            content: "mercedes",
+            score: 0,
+            used: false
+        },
+        {
+            id: 5,
+            content: "honda",
+            score: 0,
+            used: false
+        },
+        {
+            id: 6,
+            content: "tesla",
+            score: 0,
+            used: false
+        },
+        {
+            id: 7,
+            content: "toyota",
+            score: 0,
+            used: false
+        },
+        {
+            id: 8,
+            content: "crysler",
             score: 0,
             used: false
         }
-        setA1(newAnswer)
-        console.log(a1.content)
+    ]*/
+
+    const [a1,setA1] = useState(answerList[Math.floor(Math.random()*answerList.length)])
+
+    const [a2,setA2] = useState(answerList[Math.floor(Math.random()*answerList.length)])
+
+    function getNextAnswer(answer) {
+
+        if(answer == a1) {
+            
+            setA2(answerList[Math.floor(Math.random()*answerList.length)])
+        }
+        else if(answer == a2) {
+            
+            setA1(answerList[Math.floor(Math.random()*answerList.length)])
+        }
     }
 
     return (
         <>
-        <div onClick={getNextAnswer} className="answers" id="a1">
+        <div onClick={() => {getNextAnswer(a1)}} className="answers" id="a1">
           <span>{a1.content}</span>
         </div>
 
-        <div onClick={getNextAnswer} className="answers" id="a2">
+        <div onClick={() => {getNextAnswer(a2)}} className="answers" id="a2">
           <span>{a2.content}</span>
         </div>
         </>
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = new express()
+const db = require('better-sqlite3')('Things.db')
+
+app.use(express.static('./client/build'))
+app.use(express.json())
+
+app.get('/things',(req,res) =>{
+    const query = db.prepare("SELECT * FROM things")
+    const things = query.all()
+    res.json({
+        things
+    })
+})
+
+app.listen(8080,() => {
+    console.log("server started")
+})
